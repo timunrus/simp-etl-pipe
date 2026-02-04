f69e51a652c2bef8566349c382489f2d939c4025
timur <timun644@gmail.com>
2026-02-04 22:07:02 +0300

    add simple transform to fetch_posts.py. gitignore added

--- fetch_posts.py
+++ fetch_posts.py
@@ -6,8 +6,18 @@ response.raise_for_status()
 
 posts = response.json()
 
-print(f"Кол-во записей = {len(posts)}")
-print(f"Первая запись - {posts[0]}")
+# print(f"Кол-во записей = {len(posts)}")
+# print(f"Первая запись - {posts[0]}")
+
+transformed_posts = []
 
 for post in posts:
-  new_dic
+    new_dict = {
+        "post_id": post["id"],
+        "author_id": post["userId"],
+        "title": post["title"]
+    }
+    transformed_posts.append(new_dict)
+
+print(f"Кол-во записей в измененном списке = {len(transformed_posts)}")
+print(f"Первая запись в новом списке - {transformed_posts[0]}")
