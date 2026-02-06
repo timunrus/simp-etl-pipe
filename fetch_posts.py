65215984fc4bbe733f29e42333ccc3d7ef28ba72
timur <timun644@gmail.com>
2026-02-06 14:13:54 +0300

    Load posts into Postgres

--- fetch_posts.py
+++ fetch_posts.py
@@ -1,4 +1,5 @@
 import requests
+import psycopg2
 
 URL = "https://jsonplaceholder.typicode.com/posts"
 response = requests.get(URL)
@@ -19,5 +20,32 @@ for post in posts:
     }
     transformed_posts.append(new_dict)
 
-print(f"Кол-во записей в измененном списке = {len(transformed_posts)}")
-print(f"Первая запись в новом списке - {transformed_posts[0]}")
+# print(f"Кол-во записей в измененном списке = {len(transformed_posts)}")
+# print(f"Первая запись в новом списке - {transformed_posts[0]}")
+
+conn = psycopg2.connect(
+    host="localhost",
+    port=5432,
+    dbname="de_db",
+    user="de_user",
+    password="de_password"
+)
+
+cursor = conn.cursor()
+
+insert_query = """
+INSERT INTO posts (post_id, author_id, title)
+VALUES (%s, %s, %s)
+ON CONFLICT (post_id) DO NOTHING;
+"""
+
+for post in transformed_posts:
+    cursor.execute(
+        insert_query,
+        (post["post_id"], post["author_id"], post["title"])
+    )
+conn.commit()
+cursor.close()
+conn.close()
+
+print("Данные загружены в БД")
